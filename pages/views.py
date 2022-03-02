04cef5e28ad2aa666fe75bcb159fa55385728645
idu <vipteam064@gmail.com>
2022-03-02 10:31:06 +0530

    second commit

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def icons_test_view(request):
-    return render(request, 'pages/icons_test.html')
+    context = {}
+    return render(request, 'pages/icons_test.html', context)
