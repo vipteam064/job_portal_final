62c95ff1bf0606aa736dc7010050e68a4d527963
vipteam064 <vipteam064@gmail.com>
2022-04-10 17:55:54 +0530

    fourth commit

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def icons_test_view(request):
+def home_view(request):
     context = {}
-    return render(request, 'pages/icons_test.html', context)
+    return render(request, 'pages/visitor_home.html', context)
--- users/views.py
+++ users/views.py
@@ -1,110 +1,97 @@
 from django.shortcuts import render, redirect, HttpResponse
-from django.contrib.auth import authenticate, login, logout
+from django.contrib import auth, messages
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.utils import timezone
 from .models import *
 
 # Create your views here.
+@user_passes_test(lambda user: not user.is_authenticated, login_url='/', redirect_field_name=None)
 def signup_view(request):
     context = {}
     if request.method == 'POST':
-        email = request.POST.get('email')
-        pwd = request.POST.get('pwd')
-        pwd1 = request.POST.get('pwd1')
-        role = Role_master.objects.get(role_name=request.POST.get('role'))
-
-        try:
-            if pwd == pwd1:
-                user = User_account(email=email, role=role)
-                user.set_password(pwd)
-                user.full_clean()
-                user.save()
-                user = authenticate(request, email=email, password=pwd)
-                if user is not None:
-                    login(request, user)
-                    user.email_user('Account Created!', 'You have been successfully registered to job portal.')
+        role_input = request.POST.get('role')
+        email_input = request.POST.get('email')
+        password_input = request.POST.get('password')
+        confirm_password_input = request.POST.get('confirm_password')
+        context['form'] = {
+            'role': role_input,
+            'email': email_input,
+        }
+        if role_input == "JOB SEEKER" or role_input == "EMPLOYER":
+            role = Role_master.objects.get(role_name = role_input)
+            try:
+                if password_input == confirm_password_input:
+                    user_account = User_account(email=email_input, role=role)
+                    try:
+                        validate_password(password_input, user_account)
+                    except ValidationError as e:
+                        raise ValidationError({'password': e}) # converting error_list to error_dict with key 'password'
+                    user_account.set_password(password_input)
+                    user_account.full_clean()
+                    user_account.save()
+                    user_account = auth.authenticate(request, email=email_input, password=password_input)
+                    auth.login(request, user_account)
+                    user_account.email_user('Account Created!', 'You have been successfully registered to job portal.')
+                    if user_account.role.role_name == 'JOB SEEKER':
+                        return HttpResponse('personal details')
+                    else:
+                        return HttpResponse('employer details')
                 else:
-                    return redirect('/users/login')
-                if user.role.role_name == 'JOB SEEKER':
-                    pass
-                    # return redirect('personal details')
-                elif user.role.role_name == 'EMPLOYER':
-                    pass
-                    # return redirect('employer details')
-                elif user.role.role_name == 'INTERVIEWER':
-                    pass
-                    # return redirect('interviewer home')
+                    raise ValidationError({'confirm_password': 'Password and confirm password don\'t match.'})
+            except ValidationError as e:
+                if hasattr(e, 'error_dict'):
+                    context['errors'] = e.message_dict.items()
                 else:
-                    pass
-                    # return redirect('home')
-                return HttpResponse('home')
-            else:
-                raise ValidationError({'password': 'Password and confirm password don\'t match.'})
-        except ValidationError as e:
-            context['errors'] = e.message_dict.items()
-
+                    context['errors'] = [('__all__', e), ]
+        else:
+            context['errors'] = [('role', ['Please select either job seeker or employer.'])]
     return render(request, 'users/signup.html', context)
 
 def login_view(request):
     context = {}
     if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('pwd')
-        user = authenticate(request, email=email, password=password)
-        if user is not None:
-            login(request, user)
-            if user.role.role_name == "JOB SEEKER":
-                pass
-                # return redirect('jobseekerhome')
-            elif user.role.role_name == "EMPLOYER":
-                pass
-                # return redirect('employerhome')
-            elif user.role.role_name == "INTERVIEWER":
-                pass
-                # return redirect('interviewerhome')
-            elif user.role.role_name == "ADMIN" or user.role.role_name == "STAFF":
-                pass
-                # return redirect('admin:index')
+        email_input = request.POST.get('email')
+        password_input = request.POST.get('password')
+        user_account = auth.authenticate(request, email=email_input, password=password_input)
+        if user_account is not None:
+            auth.login(request, user_account)
+            redirect_url = request.GET.get('next')
+            if redirect_url:
+                return redirect(redirect_url)
             else:
-                pass
-                # return redirect('home')
-            return HttpResponse('home')
+                if user_account.role.role_name == "ADMIN" or user_account.role.role_name == "STAFF":
+                    return redirect('/admin/')
+                else:
+                    return HttpResponse('home')
         else:
             context['errors'] = [('__all__', ['Invalid email or password!']), ]
     return render(request, 'users/login.html', context)
 
 def logout_view(request):
-    logout(request)
-    return redirect('/users/login')
+    auth.logout(request)
+    return redirect('/')
 
+@login_required
 def change_email_view(request):
     context = {}
-    print(request.POST)
-    print(request.user)
     if request.method == 'POST':
         try:
-            email = request.POST.get('email')
-            user = User_account.objects.get(id=request.user.id)
-            old_email = user.email
-            user.email = email
-            user.full_clean()
-            User_account.objects.get(id=request.user.id).email_user('Email Changed!', 'Your registered email has been changed from {} to {}.'.format(old_email.lower(), user.email.lower()))
-            user.save()
-            if user.role.role_name == "JOB SEEKER":
-                pass
-                # return redirect('jobseekerhome')
-            elif user.role.role_name == "EMPLOYER":
-                pass
-                # return redirect('employerhome')
-            elif user.role.role_name == "INTERVIEWER":
-                pass
-                # return redirect('interviewerhome')
-            elif user.role.role_name == "ADMIN" or user.role.role_name == "STAFF":
-                pass
-                # return redirect('admin:index')
+            email_input = request.POST.get('email').strip().lower()
+            user_account = request.user
+            curr_email = user_account.email.lower()
+            if email_input != curr_email:
+                user_account.email = email_input
+                user_account.full_clean()
+                User_account.objects.get(id=request.user.id).email_user('Email Changed!', 'Your registered email has been changed from {} to {}.'.format(curr_email, user_account.email.lower()))
+                user_account.save()
+                messages.success(request, 'Email changed successfully!')
+                return HttpResponse('home')
             else:
-                pass
-                # return redirect('home')
-            return HttpResponse('home')
+                raise ValidationError({'email': 'New email can\'t be same as current email.'})
         except ValidationError as e:
-            context['errors'] = e.message_dict.items()
+            if hasattr(e, 'error_dict'):
+                context['errors'] = e.message_dict.items()
+            else:
+                context['errors'] = [('__all__', e), ]
     return render(request, 'users/change_email.html', context)
